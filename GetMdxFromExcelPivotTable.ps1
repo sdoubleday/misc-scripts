cfa1d9e8e7df4e430c12840f0e2bcbab13956881
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2022-10-04 19:20:38 -0400

    add scripts for getting mdx from excel pivot tables and scripting out objects from SQL Server for use in SSDT projects. remove old script

---
+++ GetMdxFromExcelPivotTable.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+Open an .xlsx file with excel (silently), get the MDX query used by the Pivot Table on the active sheet, return that, and close the workbook.
+#>>
+
+PARAM ([Parameter(Mandatory= $true,ValueFromPipelineByPropertyName= $true)][ValidateNotNullorEmpty()][ValidateScript({
+            IF (Test-Path -PathType leaf -Path $_ ) 
+                {$True}
+            ELSE {
+                Throw "$_ is not a file."
+            } 
+        })][String]$FullName)
+BEGIN{}
+PROCESS{
+$excelApp = New-Object -ComObject 'Excel.Application';
+$excelWorkBook = $excelApp.Workbooks.Open($FullName);
+$output = $excelWorkBook.ActiveSheet.PivotTables(1).MDX;
+$excelWorkBook.Close();
+
+return $output;
+}
+END {}
---
+++ ScriptSqlObjectWithDbaToolsForSSDT.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.SYNOPSIS
+SSDT Does not like SET options and insists on batch separators.
+This scripts an object without file group, with all contraints and keys, with GO batch separators, and without the standard two set options lines you get when scripting an object (ANSI_NULLS and QUOTED_IDENTIFIERS).
+These options are suitable if you are bringing table definitions into an SSDT sqlproj file to act as a reference for development of code that relies on those objects but not the rest of an overly-cumbersome source database.
+#>
+
+PARAM(
+ [String]$objectName
+,[String]$objectSchema
+,[Microsoft.SqlServer.Management.Smo.Database]$database
+)
+BEGIN   {
+Import-Module dbatools;
+$scriptingOptions = New-DbaScriptingOption;
+$scriptingOptions.DriAll = $true;
+$scriptingOptions.NoFileGroup = $true;
+$scriptingOptions.ScriptBatchTerminator = $true;
+}<# END BEGIN    #>
+PROCESS {
+$db.tables[$objectName, $objectSchema] | Export-DbaScript -PassThru -ScriptingOptionsOject $scriptingOptions | Select-Object -Skip 3; <#First object is comments, second and third are set options.#>
+}<# END PROCESS  #>
+END     {}<# END END      #>
+
